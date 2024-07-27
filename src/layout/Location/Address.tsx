ea396fe4112989751da18bd0a4cc0224c583fa4c
harrdas <bsnn2944@gmail.com>
2024-07-27 17:38:39 +0900

    - update: 가운데 정렬 수정

--- src/layout/Location/Address.tsx
+++ src/layout/Location/Address.tsx
@@ -25,7 +25,7 @@ export default Address;
 const WayWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
+  align-items: center;
   margin: 20px 0px;
   gap: 20px;
 `;
